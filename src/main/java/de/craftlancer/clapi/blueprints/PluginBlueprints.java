5eb3454f7cd59bc126dcf6339d86e38ffeee713d
Sizzle <57070735+SizzleMcGrizzle@users.noreply.github.com>
2021-08-26 13:09:33 -0500

    Some documentation

--- src/main/java/de/craftlancer/clapi/blueprints/PluginBlueprints.java
+++ src/main/java/de/craftlancer/clapi/blueprints/PluginBlueprints.java
@@ -6,10 +6,20 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PluginBlueprints {
-    
+
+    /**
+     * @return A list of all blueprints, including but not limited to player blueprints
+     */
     List<? extends AbstractBlueprint> getBlueprints();
-    
+
+    /**
+     * @param schematic the schematic the sought upon blueprint would use.
+     * @return An optional that, if present, contains a blueprint.
+     */
     Optional<? extends AbstractBlueprint> getBlueprint(String schematic);
-    
+
+    /**
+     * @return An optional that, if pressent, contains a blueprint with matching item.
+     */
     Optional<? extends AbstractBlueprint> getBlueprint(ItemStack item);
 }
